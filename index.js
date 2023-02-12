016b0c3d04a4aa5a4f5bdc04a70fe17da43ecbf2
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-02-12 16:39:54 -0500

    primary questions

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-// imports 
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const consoleTable = require('console.table');
+// imports
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
 
-const connection = require('./config/connections');
-require('dotenv').config()
-
+const connection = require("./config/connections");
+require("dotenv").config();
 
 // Connects to database
 const db = mysql.createConnection(
-    {
-      host: connection.db_host,
-      // MySQL username,
-      user: 'root',
-      // TODO: Add MySQL password
-      password: 'jphunc',
-      database: "employee_db",
-    },
-    console.log(`Connected to the database.`)
-  );
+  {
+    host: connection.db_host,
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password
+    password: "****",
+    database: "employee_db",
+  },
+  console.log(`Connected to the database.`)
+);
+
+const primaryQuestions = [
+  {
+    type: "list",
+    message: "Choose an option.",
+    name: "userChoice",
+    choices: [
+      "View all employees",
+      "Add employee",
+      "Update employee role",
+      "View all roles",
+      "Add role",
+      "View all departments",
+      "Add department",
+      "Quit",
+    ],
+    default: [0],
+    suffix: " Use arrow keys to select your choice.",
+  },
+];
+
+function mainMenu() {
+  inquirer.prompt(primaryQuestions).then((choice) => {
+    console.log(choice);
+  });
+};
+
+mainMenu();
