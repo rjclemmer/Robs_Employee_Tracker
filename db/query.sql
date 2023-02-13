98a3efb29648a6c3eefc196b2bee4ad114465ba6
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-02-13 12:49:56 -0500

    trying to add department

--- db/query.sql
+++ db/query.sql
@@ -2,8 +2,14 @@
 -- FROM role
 -- JOIN department ON role.department_id = department.id;
 
-SELECT role.id AS Id, role.title AS Role, role.salary AS Salary, department.name AS department
-From Role
-JOIN department ON role.department_id = department.id
+-- SELECT role.id AS Id, role.title AS Role, role.salary AS Salary, department.name AS department
+-- From Role
+-- JOIN department ON role.department_id = department.id
 
 -- , department.name AS Dept FROM department JOIN department ON role.department_id = department.id;
+
+SELECT employee.id AS ID, CONCAT(employee.first_name, ' ', employee.last_name) AS Name, role.title AS Role, role.salary AS Salary, department.name AS Department, IF(employee.manager_id IS NULL, 'Manager', CONCAT(employee2.first_name,' ', employee2.last_name)) AS Manager
+From employee
+JOIN role ON employee.role_id = role.id
+JOIN department on role.department_id = department.id
+LEFT JOIN employee employee2 ON employee.manager_id = employee2.id
--- index.js
+++ index.js
@@ -40,6 +40,27 @@ const primaryQuestions = [
   },
 ];
 
+ function addDepartment() {
+    const departmentName =  inquirer.prompt({
+      name: "department",
+      type: "input",
+      message: "What department are you adding",
+    });
+    const data = departmentName.department;
+    const query =   db.query(
+        "INSERT INTO department SET ?",
+        {
+          dept: data,
+        },
+    
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " Department Added\n");
+          mainMenu();
+        });
+    };
+
+
 // Main Menu Function
 function mainMenu() {
   inquirer.prompt(primaryQuestions).then((choice) => {
@@ -54,7 +75,7 @@ function mainMenu() {
     // View All departments
     if (choice.userChoice === "View all departments") {
       console.log("Viewing all Departments:");
-      let sqlQuery = `SELECT name FROM department;`;
+      let sqlQuery = `SELECT department.name as Name, department.id as ID FROM department;`;
 
       db.query(sqlQuery, (err, res) => {
         if (err) {
@@ -86,22 +107,44 @@ function mainMenu() {
     };
 
     // View All employees
-    if (choice.userChoice === "View all roles") {
-        console.log("Viewing all Roles:");
-        let sqlQuery = `SELECT role.id AS Id, role.title AS Role, role.salary AS Salary, department.name AS department
-        From Role
-        JOIN department ON role.department_id = department.id`;
+    if (choice.userChoice === "View all employees") {
+        console.log("Viewing all Employees:");
+        let sqlQuery = `SELECT employee.id AS ID, CONCAT(employee.first_name, ' ', employee.last_name) AS Name, role.title AS Role, role.salary AS Salary, department.name AS Department, IF(employee.manager_id IS NULL, 'Manager', CONCAT(employee2.first_name,' ', employee2.last_name)) AS Manager
+        From employee
+        JOIN role ON employee.role_id = role.id
+        JOIN department on role.department_id = department.id
+        LEFT JOIN employee employee2 ON employee.manager_id = employee2.id
+        `;
   
         db.query(sqlQuery, (err, res) => {
           if (err) {
             console.log("\n I'm sorry Dave. I'm afraid I can't do that.");
             return mainMenu();
           }
-          console.log("ROLES!");
+          console.log("EMPLOYEES!");
           console.table(res);
           mainMenu();
         });
       };
+
+     // Add Department
+     if (choice.userChoice === "Add department") {
+        console.log("Adding department:");
+        addDepartment();
+        // let sqlQuery = `INSERT INTO department name VALUES (${data});`;
+  
+        // db.query(sqlQuery, (err, res) => {
+        //   if (err) {
+        //     console.log("\n I'm sorry Dave. I'm afraid I can't do that.");
+        //     return mainMenu();
+        //   }
+        //   console.log("EMPLOYEES!");
+        //   console.table(res);
+        //   mainMenu();
+        // });
+      };
+
+     // let queryReq = 
   });
 }
 mainMenu();
